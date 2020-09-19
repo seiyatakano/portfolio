805bcb345b6ac7488e099b3a19ec7c4a30636d4c
seiyatakano <takanosonyes@gmail.com>
2020-09-19 18:30:23 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+$(function() {
+    var $container = $('.container_portfolios');
+
+        $.getJSON('./data/content.json', function (data) {
+            var elements = [];
+
+            $.each(data, function(i, item){
+                var itemHTML = 
+                '<div class="portfolio">'+
+                "<img src="+item.images.thumb+">"+
+                "</div>";
+                elements.push($(itemHTML).get(0));
+            });
+
+            $.each(data, function(i, item){
+                $container.append(elements[i]);
+            });
+        });
+})
