9580db2e5d5ad7f0eb1cf0313c93dc41623ab71e
seiyatakano <takanosonyes@gmail.com>
2020-09-20 13:27:55 +0900

    PC用のレイアウト

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(function() {
-    console.log('attempt4');
+    console.log('attempt5');
     var $container = $('.portfolios');
 
         $.getJSON('./data/content.json', function (data) {
