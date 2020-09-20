8dc8ea34d76da2aed099f7598623961b9d1f6ea0
seiyatakano <takanosonyes@gmail.com>
2020-09-20 12:22:40 +0900

    UIの微小調整

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ $(function() {
                     <div class="description">
                         <h3>${item.title}</h3>
                         <p>${item.description}</p>
-                        <p><a href=${item.images.url}>リンクに飛ぶ</a></p>
+                        <p><a href=${item.images.url}>${item.images.url}</a></p>
                     </div>
                 </div>`;
                 elements.push($(itemHTML).get(0));
